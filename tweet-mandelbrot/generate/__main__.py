df0a455771df17bf3ffb50ef774be99124a25de8
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-19 14:21:00 -0700

    tweak params

--- tweet-mandelbrot/generate/__main__.py
+++ tweet-mandelbrot/generate/__main__.py
@@ -60,10 +60,10 @@ def main():
     num_attempts = 0
     dur_t0 = time.time()
     std = 0  # standard deviation
-    while std < 100:  # This essentially checks the noise of the image (if 0 -> all uniform, aka a blank image)
+    while std < 70:  # This essentially checks the noise of the image (if 0 -> all uniform, aka a blank image)
         num_attempts += 1
         if (time.time() - dur_t0) > 600: break  # Guard
-        if (int(time.time() - dur_t0) % 10) == 0: printer(f'INFO: num_attempts: {num_attempts}')
+        # if (int(time.time() - dur_t0) % 10) == 0: printer(f'INFO: num_attempts: {num_attempts}')
         xmin, xmax, ymin, ymax, frame_width, frame_height = get_random_range()
         ## Render
         raw = get_raw_grayscale_image(
