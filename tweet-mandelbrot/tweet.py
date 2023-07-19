144ccfa8150e0012a5ef2622f5367149157191e2
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-19 07:28:21 -0700

    rerun

--- tweet-mandelbrot/tweet.py
+++ tweet-mandelbrot/tweet.py
@@ -40,10 +40,6 @@ def send_tweet_with_image(text, image):
     auth.set_access_token(access_token, access_token_secret)
 
     api = tweepy.API(auth)
-
-    user = api.me()
-    print('Name: ' + user.name)
-
     api.update_with_media(image, text)
 
 
