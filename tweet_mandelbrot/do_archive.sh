86b92a9514f8f5513d7c1a8b5761b7184e38bf3e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 07:09:19 -0700

    test exit if file already exists

--- tweet_mandelbrot/do_archive.sh
+++ tweet_mandelbrot/do_archive.sh
@@ -9,10 +9,24 @@ img_path="$(pwd)/tweet_mandelbrot/draft/result.png"
 echo "INFO: Deleting '$img_path'."
 # rm img_path
 
-# Move the metadata
+# Metadata
 src="$(pwd)/tweet_mandelbrot/draft/metadata.txt"
 dst="$(pwd)/tweet_mandelbrot/archive/$(date +%s | awk '{printf "%011d\n", $1}')-$TWEET_ID.txt"
+dst="$(pwd)/tweet_mandelbrot/archive/fooo.txt"
 echo "DEBUG: src: '$src'."
 echo "DEBUG: dst: '$dst'."
+
+# Change the tweet_id value in metadata.txt
+# sed -i "s/tweet_id: .*/tweet_id: $TWEET_ID/" "$src"
+
+# Check if the destination already exists or not
+if [ -e "$dst" ]; then
+    echo "ERROR: File '$dst' already exists."
+    exit 1
+fi
+
+# Move the metadata file
+echo "INFO: Moving src -> dst."
+# mv "$src" "$dst"
 
 echo "::endgroup::"
