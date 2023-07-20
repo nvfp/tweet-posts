eb282eefd41f0b203458eca779c12347581002a2
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 02:05:49 -0700

    added debugger into utils/constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -1,4 +1,7 @@
 import os
 
+from mykit.kit.utils import printer
+
 
 PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+printer(f'DEBUG: PROJECT_ROOT_DIR: {repr(PROJECT_ROOT_DIR)}.')
