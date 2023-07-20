d4d474e2c7db9858468022a04cb18ce43fbd0c32
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 02:19:42 -0700

    debugging python import module error

--- utils/register_module.py
+++ utils/register_module.py
@@ -6,6 +6,6 @@ from mykit.kit.utils import printer
 
 if __name__ == '__main__':
 
-    PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    PROJECT_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     printer(f'DEBUG: Appending {repr(PROJECT_ROOT_DIR)} to `sys.path`.')
     sys.path.append(PROJECT_ROOT_DIR)
