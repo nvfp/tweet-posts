fb367a5b130b4c3ff3cfcd499ab687dc148ed7a2
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 06:29:31 -0700

    test new archive format file name

--- tweet_mandelbrot/do_archive.sh
+++ tweet_mandelbrot/do_archive.sh
@@ -7,18 +7,12 @@ echo "::group::Archiving"
 # Delete the image
 img_path="$(pwd)/tweet_mandelbrot/draft/result.png"
 echo "INFO: Deleting '$img_path'."
-rm img_path
+# rm img_path
 
-# 
-file_name="$(date +%s | awk '{printf "%011d\n", $1}')-${{ steps.tweet.outputs.tweet_id }}.txt"
-
+# Move the metadata
+src="$(pwd)/tweet_mandelbrot/draft/metadata.txt"
+dst="$(pwd)/tweet_mandelbrot/archive/$(date +%s | awk '{printf "%011d\n", $1}')-${{ steps.tweet.outputs.tweet_id }}.txt"
+echo "DEBUG: src: '$src'."
+echo "DEBUG: dst: '$dst'."
 
 echo "::endgroup::"
-
-    # metadata_file_name = [f for f in os.listdir(DRAFT_DIR) if f.endswith('.txt')][0]
-    # metadata_file_path = os.path.join(DRAFT_DIR, metadata_file_name)
-    # src = metadata_file_path
-    # dst = os.path.join(ARCHIVE_DIR, metadata_file_name)
-    # if os.path.exists(dst): raise FileExistsError(f'Already exists: {repr(dst)}.')
-    # printer(f'INFO: Moving {repr(src)} to {repr(dst)}.')
-    # shutil.move(src, dst)
