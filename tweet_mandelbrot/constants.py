ccf3f099a3c42cb6c7157d27f83fafa2b050fb75
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 02:33:31 -0700

    bug fixed for constants.py (missing tweet_mandelbrot/ folder)

--- tweet_mandelbrot/constants.py
+++ tweet_mandelbrot/constants.py
@@ -5,6 +5,6 @@ from mykit.kit.utils import printer
 from utils.constants import PROJECT_ROOT_DIR
 
 
-ARCHIVE_DIR = os.path.join(PROJECT_ROOT_DIR, 'archive')
-DRAFT_DIR   = os.path.join(PROJECT_ROOT_DIR, 'draft')
+ARCHIVE_DIR = os.path.join(PROJECT_ROOT_DIR, 'tweet_mandelbrot', 'archive')
+DRAFT_DIR   = os.path.join(PROJECT_ROOT_DIR, 'tweet_mandelbrot', 'draft')
 printer(f'DEBUG: ARCHIVE_DIR: {repr(ARCHIVE_DIR)}.')
