7175cb6bbf974f0da24c427394cfa601dbbf8b98
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 05:27:47 -0700

    Changing the archiving mechanism

---
+++ tweet_mandelbrot/do_archive.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e  # Exit the script immediately if any command exits with a non-zero status
+
+
+echo "::group::Archiving"
+
+# Delete the image
+
+# 
+
+
+echo "::endgroup::"
+
+    # img = os.path.join(DRAFT_DIR, 'result.png')
+    # printer(f'INFO: Deleting {repr(img)}.')
+    # os.remove(img)
+
+    # metadata_file_name = [f for f in os.listdir(DRAFT_DIR) if f.endswith('.txt')][0]
+    # metadata_file_path = os.path.join(DRAFT_DIR, metadata_file_name)
+    # src = metadata_file_path
+    # dst = os.path.join(ARCHIVE_DIR, metadata_file_name)
+    # if os.path.exists(dst): raise FileExistsError(f'Already exists: {repr(dst)}.')
+    # printer(f'INFO: Moving {repr(src)} to {repr(dst)}.')
+    # shutil.move(src, dst)
