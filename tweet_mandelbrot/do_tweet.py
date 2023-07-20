2c6399a91b40d9d8c41d275df22da5f0e53772a9
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-20 05:09:11 -0700

    removed reply and metadata post in do_tweet.py

--- tweet_mandelbrot/do_tweet.py
+++ tweet_mandelbrot/do_tweet.py
@@ -11,7 +11,7 @@ _REPO_ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 printer(f'DEBUG: Appending {repr(_REPO_ROOT_DIR)} to `sys.path`.')
 sys.path.append(_REPO_ROOT_DIR)
 
-from utils.tweet import send_tweet_with_image_then_reply
+from utils.tweet import send_tweet_with_image
 from tweet_mandelbrot.constants import DRAFT_DIR
 
 
@@ -19,21 +19,6 @@ if __name__ == '__main__':
 
     ## Image path
     IMG = os.path.join(DRAFT_DIR, 'result.png')
-
-    ## Metadata
-    md_file_name = [f for f in os.listdir(DRAFT_DIR) if f.endswith('.txt')][0]
-    md_file_path = os.path.join(DRAFT_DIR, md_file_name)
-
-    ## Parse some metadata to post
-    with open(md_file_path, 'r') as f:
-        md_text = f.read()
-    md_id = re.search(r'id: (?P<id>.*)', md_text).group('id')
-    md_xmin = re.search(r'xmin: (?P<xmin>.*)', md_text).group('xmin')
-    md_xmax = re.search(r'xmax: (?P<xmax>.*)', md_text).group('xmax')
-    md_ymin = re.search(r'ymin: (?P<ymin>.*)', md_text).group('ymin')
-    md_ymax = re.search(r'ymax: (?P<ymax>.*)', md_text).group('ymax')
-    md_n_iter = re.search(r'n_iter: (?P<n_iter>.*)', md_text).group('n_iter')
-    md_antialiasing_is_on = re.search(r'antialiasing_is_on    : (?P<antialiasing_is_on>.*)', md_text).group('antialiasing_is_on')
 
     ## Tweeting
     day = datetime.now().strftime('%A')
@@ -58,18 +43,7 @@ if __name__ == '__main__':
         f'Let the magic of {day} fill your heart!',
         f'You rock, and so does this {day}!',
         f'Shine bright like the sun on this {day}!',
-        f'Wishing you a productive {day}!',
+        f'Wishing you a wonderful {day}!',
     ]
-    tweet = (
-        f'{random.choice(random_greet)} — image id: {md_id}'
-    )
-    reply = (
-        '@nvfastplease\n'
-        'metadata:\n'
-        f'real:\n{[float(md_xmin), float(md_xmax)]}\n'
-        f'imag:\n{[float(md_ymin), float(md_ymax)]}\n'
-        f'#iteration: {md_n_iter}\n'
-        f'antialiasing: {md_antialiasing_is_on}\n'
-        f'archive: https://github.com/nvfp/tweet-posts/blob/main/tweet_mandelbrot/archive/{md_id}.txt'
-    )
-    send_tweet_with_image_then_reply(tweet, IMG, reply)
+    tweet = random.choice(random_greet)
+    send_tweet_with_image(tweet, IMG)
