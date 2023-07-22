1535d81dd3587ad6e51e58a0acd21dbf16f66c23
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-21 21:51:07 -0700

    Tweak Newton resolution and disable its antialiasing to reduce compute time

--- tweet_newton/generate/__main__.py
+++ tweet_newton/generate/__main__.py
@@ -44,9 +44,11 @@ def get_random_range():
 
 def main():
 
-    w, h = 1920, 1080  # should 16:9 AR
+    # w, h = 1920, 1080  # should 16:9 AR. Note: FHD could take so long to be done
+    w, h = 1280, 720
 
-    antialiasing_is_on = random.choice([True, False])
+    # antialiasing_is_on = random.choice([True, False])
+    antialiasing_is_on = False
     antialiasing_supsample = 2
 
     n_iter = random.randint(32, 128)
