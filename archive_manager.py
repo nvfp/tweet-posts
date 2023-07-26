417043cbc6a734e217f662c2a09fbbfdad77b4b1
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-26 07:53:08 -0700

    init

---
+++ archive_manager.py
@@ -0,0 +1,11 @@
+import os
+
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+METADATA = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '__archive_temp_dir__')
+print(f'DEBUG: ')
+print(f'DEBUG: ')
+
+
+if __name__ == '__main__':
+
