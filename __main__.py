51fb45bc61d00f52e363150cc36fa9bdcbbed79c
Nicholas Valentinus <nvfastplease@gmail.com>
2024-02-26 13:13:47 -0800

    this is a dev commit: 1 file changed, 4 insertions(+), 4 deletions(-)
     __main__.py | 8 ++++----
    
    message generated by QWD (github.com/nvfp/qwd)

--- __main__.py
+++ __main__.py
@@ -1,8 +1,8 @@
 import sys, os
 sys.path.append(os.path.dirname(__file__))  # make the folders importable
 from core.fractal_burning_ship import run_burning_ship
-from core.fractal_mandelbrot import run_mandelbrot
-from core.fractal_multibrot3 import run_multibrot3
+# from core.fractal_mandelbrot import run_mandelbrot
+# from core.fractal_multibrot3 import run_multibrot3
 from core.fractal_newton import run_newton
 
 def get_fractal_name():
@@ -13,8 +13,8 @@ def get_fractal_name():
 def main():
     name = get_fractal_name()
     if name == 'BurningShip': run_burning_ship()
-    elif name == 'MandelbrotSet': run_mandelbrot()
-    elif name == 'Multibrot3': run_multibrot3()
+    # elif name == 'MandelbrotSet': run_mandelbrot()
+    # elif name == 'Multibrot3': run_multibrot3()
     elif name == 'Newton': run_newton()
     else: raise AssertionError(f"Invalid: {name}")
 
