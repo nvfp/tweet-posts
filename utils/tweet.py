e6cf7081fbb7d070a26321823b7c0e3bac4ac3e4
Nicholas Valentinus <nvfastplease@gmail.com>
2024-02-26 07:12:04 -0800

    this is a dev commit: 1 file changed, 7 insertions(+), 13 deletions(-)
     utils/tweet.py | 20 +++++++-------------
    
    message generated by QWD (github.com/nvfp/qwd)

--- utils/tweet.py
+++ utils/tweet.py
@@ -1,26 +1,21 @@
 import os
 import tweepy
 
-from mykit.kit.utils import printer
-
-
 def tweet(text, image):
     printer('INFO: Sending tweet.')
 
-    consumer_key = os.environ['TWITTER_API_KEY']
-    consumer_secret = os.environ['TWITTER_API_SECRET_KEY']
-    access_token = os.environ['TWITTER_ACCESS_TOKEN']
-    access_token_secret = os.environ['TWITTER_ACCESS_TOKEN_SECRET']
+    access_token = os.environ['X_ACCESS_TOKEN_SECRET']
+    access_token_secret = os.environ['X_ACCESS_TOKEN_SECRET']
+    consumer_key = os.environ['X_API_KEY']
+    consumer_secret = os.environ['X_API_KEY_SECRET']
 
     auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret,)
-
     client_v1 = tweepy.API(auth)
+
     client_v2 = tweepy.Client(
-        consumer_key=consumer_key,
-        consumer_secret=consumer_secret,
-        access_token=access_token,
-        access_token_secret=access_token_secret,
+        access_token=access_token, access_token_secret=access_token_secret,
+        consumer_key=consumer_key, consumer_secret=consumer_secret
     )
 
     try:
@@ -33,7 +28,6 @@ def tweet(text, image):
         printer(f'INFO: Sent with tweet_id {repr(tweet_id)}.')
 
         return tweet_id
-
     except Exception as err:
         printer(f'ERROR: {err}')
         return 'FAIL'
