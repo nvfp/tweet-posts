06f23c75ac9716bf8a911c4b936674bf2e8ed395
Nicholas Valentinus <nvfastplease@gmail.com>
2024-05-01 00:57:32 -0700

    deleted shared.py, now all constants bring closer to where it's used ── qwd(20240501_b11b351): (2fc,19+,40-,20240501_075732z0) [__main__.py (19+,21-)] [core/shared.py (gone) (0+,19-)]

--- __main__.py
+++ __main__.py
@@ -1,24 +1,22 @@
-# import sys, os
-# sys.path.append(os.path.dirname(__file__))  # make the folders importable
-# from core.fractal_burning_ship import run_burning_ship
-# from core.fractal_mandelbrot import run_mandelbrot
-# from core.fractal_multibrot3 import run_multibrot3
-# from core.fractal_newton import run_newton
+import sys, os
+sys.path.append(os.path.dirname(__file__))  # make the folders importable
+from core.fractal_burning_ship import run_burning_ship
+from core.fractal_mandelbrot import run_mandelbrot
+from core.fractal_multibrot3 import run_multibrot3
+from core.fractal_newton import run_newton
 
-# def get_fractal_name():
-#     args = sys.argv
-#     if len(args) != 2: raise AssertionError(f"Need 1 positional arg, the fractal name.")
-#     return args[1]
+def get_fractal_name():
+    args = sys.argv
+    if len(args) != 2: raise AssertionError(f"Need 1 positional arg, the fractal name.")
+    return args[1]
 
-# def main():
-#     name = get_fractal_name()
-#     if name == 'BurningShip': run_burning_ship()
-#     elif name == 'MandelbrotSet': run_mandelbrot()
-#     elif name == 'Multibrot3': run_multibrot3()
-#     elif name == 'Newton': run_newton()
-#     else: raise AssertionError(f"Invalid: {name}")
+def main():
+    name = get_fractal_name()
+    if name == 'BurningShip': run_burning_ship()
+    elif name == 'MandelbrotSet': run_mandelbrot()
+    elif name == 'Multibrot3': run_multibrot3()
+    elif name == 'Newton': run_newton()
+    else: raise AssertionError(f"Invalid: {name}")
 
-# if __name__ == '__main__':
-#     main()
-
-print(123123)
+if __name__ == '__main__':
+    main()
