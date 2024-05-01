74fb336e22ec4b0ddc6aaa626829eefe05ec071f
Nicholas Valentinus <nvfastplease@gmail.com>
2024-05-01 01:03:03 -0700

    qwdCommit ── qwd(20240501_b11b351): (1fc,8+,20-,20240501_080303z0) [__main__.py (8+,20-)]

--- __main__.py
+++ __main__.py
@@ -1,22 +1,10 @@
-import sys, os
-sys.path.append(os.path.dirname(__file__))  # make the folders importable
-from core.fractal_burning_ship import run_burning_ship
-from core.fractal_mandelbrot import run_mandelbrot
-from core.fractal_multibrot3 import run_multibrot3
-from core.fractal_newton import run_newton
-
-def get_fractal_name():
-    args = sys.argv
-    if len(args) != 2: raise AssertionError(f"Need 1 positional arg, the fractal name.")
-    return args[1]
-
-def main():
-    name = get_fractal_name()
-    if name == 'BurningShip': run_burning_ship()
-    elif name == 'MandelbrotSet': run_mandelbrot()
-    elif name == 'Multibrot3': run_multibrot3()
-    elif name == 'Newton': run_newton()
-    else: raise AssertionError(f"Invalid: {name}")
+import random
+from main.fractal_mandelbrot import runMandelbrot
+from main.fractal_multibrot3 import runMultibrot3
+from main.fractal_newton import runNewton
+from main.fractal_burning_ship import runBurningShip
 
 if __name__ == '__main__':
-    main()
+    # k=random.randint(0,3)
+    k=random.choice([run_burning_ship,run_mandelbrot,run_multibrot3,run_newton])
+    k()
